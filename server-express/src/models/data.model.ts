fa7821cfe4299d31f27b6b72eaaa4879e658996c
Jochemos <danjoch@onet.pl>
2022-01-16 11:03:01 +0100

    ADD CREATE (NEXT PART SERVICE)
    USE TYPEORM FOR CREATE NEW COMMENT BY DEFAULT USER

---
+++ server-express/src/models/data.model.ts
@@ -0,0 +1,10 @@
+export class DataModel {
+
+  commentId?: number;
+
+  author: string;
+
+  comment: string;
+
+  created_At?: Date;
+}
--- server-express/src/user/user.service.ts
+++ server-express/src/user/user.service.ts
@@ -1,11 +1,18 @@
 import { getRepository } from 'typeorm';
 import { Comment_Entity } from './user.entity';
+import { DataModel } from '../models/data.model';
 
 export class UserService {
 
-  async execute() {
+  async execute(): Promise<Comment_Entity[]> {
     const repo = await getRepository(Comment_Entity).find();
     return repo;
+  }
+
+  async createComment(comment: DataModel): Promise<any> {
+    const repo = getRepository(Comment_Entity);
+    const result = repo.create(comment);
+    await repo.save(result);
   }
 
 }
