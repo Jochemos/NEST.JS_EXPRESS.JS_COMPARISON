2abf7f63b413250ed3f22955afe7b569a7a42d34
Jochemos <danjoch@onet.pl>
2022-01-20 16:11:57 +0100

    FIX CODE (THANKS LINTER)
    INSTALL ES LINTER AND CONFIGURATION WITH AIRBNB
    + TESTS AROUND PRIVATE CUSTOM
    
    CHANGE PROJECT CATALOGS BASE MULTI-TIER ARCHITECTURE

--- server-express/src/main.ts
+++ server-express/src/main.ts
@@ -1,9 +1,8 @@
-import "reflect-metadata";
-import './app.module';
+import 'reflect-metadata';
+import './app.module.ts';
 import express from 'express';
-import { router } from './user/user.controller';
-
-require('dotenv').config();
+import router from './user/user.controller.ts';
+import 'dotenv/config';
 
 const app = express();
 app.use(express.json());
@@ -11,6 +10,6 @@ app.use(router);
 
 app.set('port', process.env.PORT_START);
 
-const server = app.listen(app.get('port'), () =>{
-  console.log(`Server work on port ${ server.address().port }`);
-})
+const server = app.listen(app.get('port'), () => {
+  console.log(`Server work on port ${server.address().port}`);
+});
--- server-express/src/user/user.controller.ts
+++ server-express/src/user/user.controller.ts
@@ -1,20 +1,25 @@
-import { Request, Response } from 'express';
-import { Router } from 'express';
-import { UserService } from './user.service';
+import { Request, Response, Router } from 'express';
+import UserService from './user.service.ts';
 
-export const router = Router();
+const router = Router();
 
-const getAll = async(request: Request, response: Response) => {
+const getAll = async (request: Request, response: Response) => {
   const findService = new UserService();
   const view = await findService.execute();
   return response.json(view);
-}
+};
 router.get('/user', getAll);
 
-const addOne = async(request: Request, response: Response) => {
-  const { commentId, author, comment, created_At } = request.body;
+const addOne = async (request: Request, response: Response) => {
+  const {
+    commentId, author, comment, createdAt,
+  } = request.body;
   const findService = new UserService();
-  const view = await findService.createComment({ commentId, author, comment, created_At });
-  return response.json({ "add": "success" });
-}
+  await findService.createComment({
+    commentId, author, comment, createdAt,
+  });
+  return response.json({ add: 'success' });
+};
 router.post('/user/addcomment', addOne);
+
+export default router;
